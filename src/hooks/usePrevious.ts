1b70355315801ec2f2b29cd9363482e0d17660f1
Akash Kumawat <akashkmt963@gmail.com>
2025-03-29 10:47:32 +0530

    Added npm pacakge details and usePrevious hook

---
+++ src/hooks/usePrevious.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * Custom hook to keep track of the previous value of a given state or prop.
+ *
+ * @template T - The type of the value being tracked
+ * @param {T} value - The current value to track
+ * @returns {T | undefined} - The previous value before the last update. Initially, it will be undefined
+ *
+ * @example
+ * const prevCount = usePreviousValue(count);
+ * console.log(prevCount); // Logs the previous count value
+ * 
+ */
+export function usePrevious<T>(value: T): T | undefined {
+  const ref = useRef<T>(undefined);
+  
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+  
+  return ref.current;
+}
